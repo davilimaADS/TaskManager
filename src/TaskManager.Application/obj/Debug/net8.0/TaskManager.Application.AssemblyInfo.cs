c151c06edf15db58a3a20c538e80fdb438fc172d
davilimaADS <daviqueirozlima6@gmail.com>
2025-06-04 13:36:16 -0300

    :bricks: build: AssemblyInfo.cs version changes witch develop branch

--- src/TaskManager.Application/obj/Debug/net8.0/TaskManager.Application.AssemblyInfo.cs
+++ src/TaskManager.Application/obj/Debug/net8.0/TaskManager.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+515a300f09a0c98e48ff98ffc37865af1dabb307")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e26a4b1a25aade2908be4d8b2c4345df3850b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
